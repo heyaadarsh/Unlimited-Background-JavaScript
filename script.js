a55f4c89d8dfca651e144eae69e50c82c9e9f496
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2024-04-26 19:50:49 +0530

    Unlimited Background Colors using setInterval() and clearInterval() of JavaScript

---
+++ script.js
@@ -0,0 +1,30 @@
+function colorName(){
+    const str = "0123456789ABCDEF";
+    let code = "#";
+
+    for(let i = 0; i<6; i++){
+        code += str[Math.floor(Math.random() * 16)];
+    }
+
+    // console.log(code);
+    return code;
+}
+
+let change;
+
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop')
+
+const changeTheme = () => {
+    change = setInterval(()=> {
+        let color = colorName();
+        document.body.style.backgroundColor = color;
+    }, 100);
+}
+
+const stopChange = () => {
+    clearInterval(change);
+}
+
+start.addEventListener('click', changeTheme);
+stop.addEventListener('click', stopChange);
